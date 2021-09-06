2b41399c3c3bc365af1c14bf970bf2f961fa4db3
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-06 16:32:48 -0300

    implement listener on main activity

--- app/src/main/java/com/lucasmezencio/todolist/MainActivity.kt
+++ app/src/main/java/com/lucasmezencio/todolist/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.lucasmezencio.todolist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lucasmezencio.todolist.databinding.ActivityMainBinding
+import com.lucasmezencio.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fabAddButton.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
