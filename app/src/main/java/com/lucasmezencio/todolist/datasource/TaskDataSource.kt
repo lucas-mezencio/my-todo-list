52834e32792ff4cfad4f2472a15b57f108e06c2b
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-06 16:00:58 -0300

    create task and task datasource

---
+++ app/src/main/java/com/lucasmezencio/todolist/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package com.lucasmezencio.todolist.datasource
+
+import com.lucasmezencio.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task) {
+        list.add(task.copy(id = list.size + 1))
+    }
+}
