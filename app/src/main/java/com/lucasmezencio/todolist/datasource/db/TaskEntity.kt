963ed2c931500fdb80908d7e96b96c374356e56f
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-21 14:12:12 -0300

    create task entity data class

---
+++ app/src/main/java/com/lucasmezencio/todolist/datasource/db/TaskEntity.kt
@@ -0,0 +1,13 @@
+package com.lucasmezencio.todolist.datasource.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)val id: Int,
+    val title: String,
+    val description: String,
+    val time: String,
+    val date: String
+)
