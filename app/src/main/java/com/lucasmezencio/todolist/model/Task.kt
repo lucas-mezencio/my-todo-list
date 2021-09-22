0f2412b7ef8c1ab0ceb622189fd9999a2397a565
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-22 19:16:36 -0300

    refactor description property

--- app/src/main/java/com/lucasmezencio/todolist/model/Task.kt
+++ app/src/main/java/com/lucasmezencio/todolist/model/Task.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class Task(
     @PrimaryKey(autoGenerate = true)val id: Int = 0,
     val title: String,
-    val description: String,
+    val description: String? = null,
     val time: String,
     val date: String
 ) {
