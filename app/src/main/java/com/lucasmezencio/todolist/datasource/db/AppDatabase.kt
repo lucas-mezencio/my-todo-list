7b4112a395b20eaa602039939503725b11db2307
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-22 19:23:24 -0300

    add database callback to prepopulate -test- data

--- app/src/main/java/com/lucasmezencio/todolist/datasource/db/AppDatabase.kt
+++ app/src/main/java/com/lucasmezencio/todolist/datasource/db/AppDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.lucasmezencio.todolist.datasource.db.dao.TaskDao
 import com.lucasmezencio.todolist.model.Task
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Task::class], version = 1, )
 abstract class AppDatabase : RoomDatabase() {
@@ -27,10 +29,36 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).build()
+                ).addCallback(AppDatabaseCallback(scope)).build()
                 INSTANCE = instance
                 return instance
             }
+        }
+    }
+
+    private class AppDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let {
+                scope.launch {
+                    populateDatabase(it.taskDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(taskDao: TaskDao) {
+            taskDao.deleteAll()
+
+            val task = Task(
+                title = "teste",
+                time = "00:00",
+                date = "01/01/2001"
+            )
+
+            taskDao.insert(task)
+            taskDao.insert(task)
         }
     }
 }
