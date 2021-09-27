9ac3ccb9742d48346238e49941310da21d367f49
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-27 18:09:04 -0300

    create add task viewmodel

---
+++ app/src/main/java/com/lucasmezencio/todolist/viewmodel/AddTaskViewModel.kt
@@ -0,0 +1,27 @@
+package com.lucasmezencio.todolist.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.lucasmezencio.todolist.datasource.repository.TaskRepository
+import com.lucasmezencio.todolist.model.Task
+import kotlinx.coroutines.launch
+
+class AddTaskViewModel(private val repository: TaskRepository) : ViewModel() {
+    fun insert(task: Task) = viewModelScope.launch {
+        repository.insert(task)
+    }
+
+    fun getTaskById(id: Int): Task = viewModelScope.launch {
+        repository.getTaskById(id)
+    }
+}
+
+class AddTaskViewModelFactory(private val repository: TaskRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AddTaskViewModel::class.java)) {
+            return AddTaskViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Classe ViewModel desconhecida")
+    }
+}
