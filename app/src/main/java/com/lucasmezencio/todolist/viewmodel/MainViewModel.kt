026f34a402471df77a27ff1c667f863c18459364
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-27 18:14:52 -0300

    refactor to delete unused methods

--- app/src/main/java/com/lucasmezencio/todolist/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/lucasmezencio/todolist/viewmodel/MainViewModel.kt
@@ -1,16 +1,15 @@
 package com.lucasmezencio.todolist.viewmodel
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.lucasmezencio.todolist.datasource.repository.TaskRepository
 import com.lucasmezencio.todolist.model.Task
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: TaskRepository) : ViewModel() {
-    val taskList: LiveData<List<Task>> = repository.getTaskList().asLiveData()
 
-    fun insert(task: Task) = viewModelScope.launch {
-        repository.insert(task)
-    }
+    val taskList: LiveData<List<Task>> = repository.getTaskList().asLiveData()
 }
 
 class MainViewModelFactory(private val repository: TaskRepository) : ViewModelProvider.Factory{
