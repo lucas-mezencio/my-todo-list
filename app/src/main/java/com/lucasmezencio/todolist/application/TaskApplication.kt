5330e80f8e5842c1920e5b6299a83310153489fa
lucas-mezencio <lucasmezss@hotmail.com>
2021-09-27 18:14:01 -0300

    change task application attributes to private

--- app/src/main/java/com/lucasmezencio/todolist/application/TaskApplication.kt
+++ app/src/main/java/com/lucasmezencio/todolist/application/TaskApplication.kt
@@ -8,8 +8,8 @@ import kotlinx.coroutines.SupervisorJob
 
 class TaskApplication : Application() {
 
-    val applicationScope = CoroutineScope(SupervisorJob())
+    private val applicationScope = CoroutineScope(SupervisorJob())
 
-    val database by lazy { AppDatabase.getDatabase(this, applicationScope) }
+    private val database by lazy { AppDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { TaskDbDataSource(database.taskDao()) }
 }
