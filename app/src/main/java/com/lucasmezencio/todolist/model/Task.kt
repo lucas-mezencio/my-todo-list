d6dcc2cc8effc2e2ecd06f2057ee1fe2df0d7bb9
Lucas Mezencio <lucasmezss@hotmail.com>
2021-09-30 14:07:21 -0300

    fix update recycler view removing custom equals and hashcode in order to diffcallback works

--- app/src/main/java/com/lucasmezencio/todolist/model/Task.kt
+++ app/src/main/java/com/lucasmezencio/todolist/model/Task.kt
@@ -10,19 +10,4 @@ data class Task(
     val description: String? = null,
     val time: String,
     val date: String
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Task
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
-}
+)
